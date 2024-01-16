641b27f5a8d6016a6aeaa3c774ab3ac681cc8095
njacques <nicholas.d.jacques@gmail.com>
2024-01-16 13:27:44 -0700

    color hit ships red

--- lib/ui/board.rb
+++ lib/ui/board.rb
@@ -13,24 +13,23 @@ module UI
     def set_content
       @board_data.grid.each_with_index do |row, y|
         @window.setpos(y,0)
-        row_string = row.map {|cell| cell.render(show_ships: @show_ships)}.join(' ')
-        @window.addstr(row_string)
+        row.each_with_index do |cell, i|
+          @window.setpos(y, i*2)
+          render_cell(cell)
+        end
       end
     end
 
-    # def render_cell(cell)
-    #   if cell.ship && cell.is_hit
-    #     'H'
-    #   elsif cell.ship && @show_ships
-    #     'S'
-    #   elsif cell.is_hit
-    #     'X'
-    #   else
-    #     ' '
-        
-
-    #   end
-    # end
+    def render_cell(cell)
+      Curses.init_pair(1, 1, 0)
+      if cell.is_hit? && cell.ship
+        @window.attrset(Curses.color_pair(1))
+        @window.addstr(cell.render(show_ships: @show_ships))
+        @window.attrset(Curses.color_pair(0))
+      else
+        @window.addstr(cell.render(show_ships: @show_ships))
+      end
+    end
 
   end
 end
