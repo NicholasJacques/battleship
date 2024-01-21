598ffeab6c1b1c905243edbcef12ac0cd10182d9
njacques <nicholas.d.jacques@gmail.com>
2024-01-21 13:49:23 -0700

    refactor fire position validator

--- lib/fire_strategies/fire_position_validator.rb
+++ lib/fire_strategies/fire_position_validator.rb
@@ -1,18 +1,36 @@
 class FirePositionValidator
+  def initialize(position, board)
+    @position = position
+    @board = board
+    @errors = []
+  end
 
   def self.validate_fire_position(position, board)
-    errors = []
+    new(position, board).validate_fire_position
+  end
 
-    if !board.cells[position]
-      errors << "Cell #{position} is not on the board."
-    end
-
-    if board.cells[position] && board.cells[position].hit
-      errors << "Cell #{position} has already been fired upon."
-    end
+  def validate_fire_position
+    validate_cell_exists
+    validate_cell_not_already_fired_upon
 
     raise FirePositionError.new(position, errors: errors) if !errors.empty?
   end
+
+  private
+  attr_reader :position, :board, :errors
+  
+  def validate_cell_not_already_fired_upon
+    if board.cells[position] && board.cells[position].hit
+      errors << "Cell #{position} has already been fired upon."
+    end
+  end
+
+  def validate_cell_exists
+    if !board.cells[position]
+      errors << "Cell #{position} is not on the board."
+    end
+  end
+
 end
 
 class FirePositionError < StandardError
